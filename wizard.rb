f8a527f53c31218905cfddf74be6a1986b48a313
Gustavo Villagrana <gvillagrana@outlook.com>
2014-06-05 15:27:50 -0600

    Bearded tests work

--- wizard.rb
+++ wizard.rb
@@ -1,11 +1,16 @@
 class Wizard
-  def initialize(name)
-    @name = name
+  def initialize(name, bearded: true)
+    @name    = name
+    @bearded = bearded
+  
   end
 
   def name
     @name
   end
 
+  def bearded?
+    @bearded
+  end
 
 end
